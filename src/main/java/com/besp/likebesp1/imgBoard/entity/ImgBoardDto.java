ae92c49dfb691d294f20dfd34c5fe77d58c47bf4
sohhhyeeun <syeankm12076@gmail.com>
2023-11-03 13:05:04 +0900

    이미지 게시판 DTO 추가

---
+++ src/main/java/com/besp/likebesp1/imgBoard/entity/ImgBoardDto.java
@@ -0,0 +1,11 @@
+package com.besp.likebesp1.imgBoard.entity;
+
+import com.besp.likebesp1.common.BaseDto;
+
+public class ImgBoardDto extends BaseDto {
+    private final long imgBoardId = 0L;
+    private final String title = "";
+    private final String imgFile = "";
+    private final String writer = "";
+    private final String createdDate = "";
+}
