783ee86a4ded73d2bc040c39d487a61ad82170d4
sohhhyeeun <syeankm12076@gmail.com>
2023-11-03 10:51:10 +0900

    DTO 생성

---
+++ src/main/java/com/besp/likebesp1/entity/ChatRoomDto.java
@@ -0,0 +1,11 @@
+package com.besp.likebesp1.entity;
+
+import com.besp.likebesp1.common.BaseDto;
+import lombok.Getter;
+
+@Getter
+public class ChatRoomDto extends BaseDto {
+    private final long chatRoomId = 0L;
+    private final String chatRoomName = "";
+    private final String createdDate = "";
+}
---
+++ src/main/java/com/besp/likebesp1/entity/CommentDto.java
@@ -0,0 +1,11 @@
+package com.besp.likebesp1.entity;
+
+import com.besp.likebesp1.common.BaseDto;
+import lombok.Getter;
+
+@Getter
+public class CommentDto extends BaseDto {
+    private final long commentId = 0L;
+    private final String content = "";
+    private final String createdDate = "";
+}
