fca0f7a38fcf2d75483d0756e3fd1f1373bfe005
yohan <sjohn9227@naver.com>
2023-11-03 11:28:08 +0900

    feat: mybatis 추가

--- src/main/java/com/besp/likebesp1/entity/MemberDto.java
+++ src/main/java/com/besp/likebesp1/entity/MemberDto.java
@@ -2,14 +2,15 @@ package com.besp.likebesp1.entity;
 
 import com.besp.likebesp1.common.BaseDto;
 import lombok.Getter;
+import lombok.ToString;
 
+@ToString
 @Getter
 public class MemberDto extends BaseDto {
-    private final Long memberId = 0L;
-    private final String userId = "";
-    private final String password = "";
-    private final String username = "";
-    private final String email = "";
-    private final String phone = "";
-    private final String createdDate = "";
+    private Long memberId = 0L;
+    private String password = "";
+    private String username = "";
+    private String email = "";
+    private String phone = "";
+    private String createdDate = "";
 }
