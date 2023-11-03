16f0dcd61a9885c09de640b26193ab1e5bb348e3
debussysuh <debussysuh@gmail.com>
2023-11-03 18:08:22 +0900

    test: 테스트코드 추가 #3

---
+++ src/test/java/com/besp/likebesp1/board/service/BoardServiceTest.java
@@ -0,0 +1,61 @@
+package com.besp.likebesp1.board.service;
+
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.besp.likebesp1.board.entity.BoardDto;
+import com.besp.likebesp1.board.repository.BoardDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardServiceTest {
+
+    private BoardServiceImpl boardService;
+    private BoardDao boardDao;
+
+    @BeforeEach
+    public void setup() {
+        boardDao = mock(BoardDao.class);
+        boardService = new BoardServiceImpl(boardDao);
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("게시판 추가")
+    public void testInsert() {
+        // given
+        BoardDto boardDto = new BoardDto();
+        boardDto.setBoardName("자유게시판");
+
+        // when
+        boardService.insert(boardDto);
+
+        // then
+        System.out.println("게시판 추가 성공");
+        verify(boardDao, times(1)).insert(boardDto);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("게시판 조회")
+    public void testGetList() {
+        // given
+        List<BoardDto> expectedList = new ArrayList<>();
+        when(boardDao.getList()).thenReturn(expectedList);
+
+        // when
+        List<BoardDto> actualList = boardService.getList();
+
+        // then
+        System.out.println("기대한 목록 크기: " + expectedList.size());
+        System.out.println("실제 목록 크기: " + actualList.size());
+
+        assertThat(actualList).isNotNull();
+        assertThat(actualList).isEqualTo(expectedList);
+    }
+}
