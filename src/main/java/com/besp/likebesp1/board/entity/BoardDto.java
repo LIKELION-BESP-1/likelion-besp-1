f24c4898ce965e847029eac54e8f9590eba46c12
debussysuh <debussysuh@gmail.com>
2023-11-04 23:36:35 +0900

    chore: 게시판 추가 시 현재 날짜가 저장되도록 수정

--- src/main/java/com/besp/likebesp1/board/entity/BoardDto.java
+++ src/main/java/com/besp/likebesp1/board/entity/BoardDto.java
@@ -3,15 +3,16 @@ package com.besp.likebesp1.board.entity;
 import com.besp.likebesp1.common.BaseDto;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
 public class BoardDto extends BaseDto {
 
     private long boardId = 0L;
-    private String boardName = "";
-    private String createdDate = "";
+    private String boardName;
+    private String createdDate = LocalDate.now().toString();
 
     public BoardDto() {
         super();
