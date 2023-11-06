7ef12bbd8d4361c09c0da86b48b99fcd47197bc6
GreenTea <sjohn9227@naver.com>
2023-11-06 13:16:38 +0900

    로그인, 로그아웃 (#11)
    * feat: member save , test code 수정
    
    * feat: member service, password encrypt 기능
    
    * refactor: 줄 정렬
    
    * feat: 로그인 및 등록 추가
    
    * feat: member mapper null 처리
    
    * feat: 로그인 필터 등록
    
    * feat: 로그인, 로그아웃시 html 처리  및 테스트
    
    * feat: 로그인,등록 api 통신으로 수정
    
    * feat: 테스트 코드 수정
    
    * feat: 로그인시 접근 권한 로직 추가
    
    * feat: enum static import
    
    * refactor: if문 구조 개선

--- src/main/java/com/besp/likebesp1/member/controller/MemberController.java
+++ src/main/java/com/besp/likebesp1/member/controller/MemberController.java
@@ -1,11 +1,23 @@
 package com.besp.likebesp1.member.controller;
 
+import com.besp.likebesp1.member.dto.MemberDto;
+import com.besp.likebesp1.member.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import static com.besp.likebesp1.common.LoginUser.LOGIN_USER;
+
+@RequiredArgsConstructor
 @Controller
 public class MemberController {
+
+    private final MemberService memberService;
 
 
     @GetMapping("/member/login")
@@ -13,8 +25,34 @@ public class MemberController {
         return "/member/login";
     }
 
+
     @PostMapping("/member/login")
-    public String postLogin(String username, String password) {
-        return "/";
+    public ResponseEntity<String> postLogin(String userId, String password, HttpServletRequest request) {
+        if (!memberService.isMember(userId, password)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        HttpSession session = request.getSession();
+        session.setAttribute(LOGIN_USER.name(), userId);
+        return new ResponseEntity<>("/", HttpStatus.OK);
     }
+
+    @GetMapping("/member/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.invalidate();
+        return "redirect:/";
+    }
+
+    @GetMapping("/member/register")
+    public String getRegister() {
+        return "/member/register";
+    }
+
+
+    @PostMapping("/member/register")
+    public ResponseEntity<String> postRegister(MemberDto dto) {
+        memberService.save(dto);
+        return new ResponseEntity<>("/", HttpStatus.OK);
+    }
+
 }
