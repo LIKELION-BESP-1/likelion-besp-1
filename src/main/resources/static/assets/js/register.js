10da22a1e7f421f87a08579ef5f36dd3b5d48062
yohan <sjohn9227@naver.com>
2023-11-07 11:27:44 +0900

    feat: alert창 수정

--- src/main/resources/static/assets/js/register.js
+++ src/main/resources/static/assets/js/register.js
@@ -1,3 +1,5 @@
+import {createAlert} from './createNode.js';
+
 $(() => {
     $("#sendButton").click(() => {
         $.ajax({
@@ -6,8 +8,17 @@ $(() => {
             data: $("#myform").serialize(),
         })
             .done((res) => {
-                alert("성공");
-                location.href = res;
+                const $blurDiv = document.createElement('div');
+                $blurDiv.classList.add("blur-background");
+
+                const $div = createAlert('회원 가입 성공 다시 로그인 해주세요', res);
+                const $body = $('body');
+                $($body).append($blurDiv);
+                $($body).append($div);
+
+                setTimeout(() => {
+                    location.href = res;
+                }, 3000);
             })
             .fail((res, status, error) => {
                 alert("잘못된 요청입니다.");
