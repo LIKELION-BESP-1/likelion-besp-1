8f5370897e1f9e4d3fb9ab93ba8362627236c6dd
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-11-07 13:55:17 +0900

    feat: ChatRoomService, ChatRoomServiceImpl 수정 - findByRoomId 메소드 추가 - createChatRoom 파라미터 수정

--- src/main/java/com/besp/likebesp1/ChatRoom/service/ChatRoomServiceImpl.java
+++ src/main/java/com/besp/likebesp1/ChatRoom/service/ChatRoomServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ChatRoomServiceImpl implements ChatRoomService {
@@ -17,13 +18,18 @@ public class ChatRoomServiceImpl implements ChatRoomService {
     }
 
     @Override
-    public void createChatRoom(ChatRoomDto chatRoom) {
-        chatRoomDao.insertChatRoom(chatRoom);
+    public void createChatRoom(ChatRoomDto chatRoomDto) {
+        chatRoomDao.insertChatRoom(chatRoomDto);
     }
 
     @Override
     public List<ChatRoomDto> getAllChatRooms() {
         return chatRoomDao.selectAllChatRooms();
+    }
+
+    @Override
+    public Optional<ChatRoomDto> findByRoomId(Long roomId) {
+        return chatRoomDao.findByRoomId(roomId);
     }
 }
 
