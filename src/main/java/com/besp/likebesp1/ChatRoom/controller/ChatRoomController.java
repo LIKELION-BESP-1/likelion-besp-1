a73ab7baff74dd85415741b508d2f1057d26143c
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-11-09 12:04:43 +0900

    feat: ChatRoomController 수정 - createRoom 로직 변경

--- src/main/java/com/besp/likebesp1/ChatRoom/controller/ChatRoomController.java
+++ src/main/java/com/besp/likebesp1/ChatRoom/controller/ChatRoomController.java
@@ -12,10 +12,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -67,10 +64,7 @@ public class ChatRoomController {
     }
 
     @PostMapping("/create")
-    public String createRoom(ChatRoomDto dto) {
-        dto.setChatRoomName(dto.getChatRoomName());
-        dto.setChatRoomContent(dto.getChatRoomContent());
-
+    public String createRoom(@ModelAttribute ChatRoomDto dto) {
         chatRoomService.createChatRoom(dto);
         return "redirect:/room/roomList";
     }
